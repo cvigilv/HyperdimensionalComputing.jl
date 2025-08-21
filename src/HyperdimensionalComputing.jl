d34da61e07e3d0970dd9b209ecbc0b0a4be2bb28
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-08-21 17:27:10 +0200

    feat: export encoders

--- src/HyperdimensionalComputing.jl
+++ src/HyperdimensionalComputing.jl
@@ -6,15 +6,19 @@ export AbstractHDV, BinaryHDV, BipolarHDV,
     GradedBipolarHDV, RealHDV, GradedHDV
 export offsetcombine, offsetcombine!
 export aggregate, aggregate!, aggregatewith!, bind, bind!, Π, Π!, resetoffset!
-export sequence_embedding, sequence_embedding!
-export compute_1_grams, compute_2_grams, compute_3_grams, compute_4_grams, compute_5_grams, 
-        compute_6_grams, compute_7_grams, compute_8_grams
 export similarity, jacc_sim, cos_sim
 export train, predict, retrain!
 
 include("vectors.jl")
 include("operations.jl")
 include("encoding.jl")
+export multiset,
+	multibind,
+	bundlesequence,
+	bindsequence,
+	hashtable,
+	crossproduct,
+	ngrams
 include("inference.jl")
 include("learning.jl")
 
