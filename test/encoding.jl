d0a3999ece4a3031c49f1cb02523ab3a17135e04
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-08-27 18:27:19 +0200

    test: cover encoders

--- test/encoding.jl
+++ test/encoding.jl
@@ -1,32 +1,58 @@
 @testset "encoding" begin
-    # TODO add more examples and types to test
-
-    hdvs = [GradedHDV(10) for i in 1:5]
-    alphabet = "STOCK"
-    basis = Dict(zip(alphabet,hdvs))
-
-    @testset "NGrams" begin
-
-        fourgrams = compute_4_grams(hdvs)
-        fourgrams_chars = compute_4_grams(basis)
-
-        @test fourgrams.d[2][1][4][3] ≈ Π(hdvs[2], 0) * Π(hdvs[1], 1) * Π(hdvs[4], 2) * Π(hdvs[3], 3)
-        @test fourgrams.d[2][1][4][3] ≈ fourgrams_chars.d['T']['S']['C']['O']
-    end 
-
-    @testset "sequence embedding" begin
-        sequence = [2, 3, 5, 1, 3, 4]
-
-        emb = sequence_embedding(sequence, hdvs, 3)
-
-        @test typeof(emb) == eltype(hdvs)
-
-        threegrams = compute_3_grams(hdvs)
-
-        @test sequence_embedding(sequence, threegrams) ≈ emb
-
-        threegrams = compute_3_grams(basis)
-
-        @test sequence_embedding([alphabet[i] for i in sequence], threegrams) ≈ emb
+    @testset "multiset" begin
+        v = [
+            BinaryHDV(v) for v in [
+                    [true, false, false, false, false],
+                    [true, true, false, false, false],
+                    [true, true, true, false, false],
+                    [true, true, true, true, false],
+                    [true, true, true, true, true],
+                ]
+        ]
+        @test multiset(v).v == [true, true, true, false, false]
+    end
+    @testset "multibind" begin
+        v = [
+            BinaryHDV(v) for v in [
+                    [true, false, false, false, false],
+                    [true, true, false, false, false],
+                    [true, true, true, false, false],
+                    [true, true, true, true, false],
+                    [true, true, true, true, true],
+                ]
+        ]
+        @test multibind(v).v == [true, false, true, false, true]
+    end
+    @testset "bundlesequence" begin
+        v = [
+            BinaryHDV(v) for v in [
+                    [true, false, false, false, false],
+                    [true, true, false, false, false],
+                    [true, true, true, false, false],
+                    [true, true, true, true, false],
+                    [true, true, true, true, true],
+                ]
+        ]
+        @test bundlesequence(v).v == [true, false, true, false, true]
+    end
+    @testset "bindsequence" begin
+        v = [
+            BinaryHDV(v) for v in [
+                    [true, false, false, false, false],
+                    [true, true, false, false, false],
+                    [true, true, true, false, false],
+                    [true, true, true, true, false],
+                    [true, true, true, true, true],
+                ]
+        ]
+        @test bindsequence(v).v == [true, false, true, false, true]
+    end
+    @testset "hashtable" begin
+    end
+    @testset "crossproduct" begin
+    end
+    @testset "ngrams" begin
+    end
+    @testset "graph" begin
     end
 end
