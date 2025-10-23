1ed6017ea747f5fe525faf99b99090aace4b4c71
michielstock <michielfmstock@gmail.com>
2025-10-23 09:14:04 +0200

    :wrench: short repr binary hv

--- src/representations.jl
+++ src/representations.jl
@@ -10,7 +10,7 @@ function Base.show(io::IO, mime::MIME"text/plain", hv::AbstractHV)
     return println(io, "mean ± std : $(round(mean(hv), digits = 3)) ± $(round(std(hv), digits = 3))")
 end
 
-function Base.show(io::IO, mime::MIME"text/plain", hv::Union{BinaryHV, BipolarHV})
+function Base.show(io::IO, mime::MIME"text/plain", hv::Union{BinaryHV,BipolarHV})
     counts = Dict(e => count(==(e), hv) for e in unique(hv))
     n = hv isa BinaryHV ? 0 : -1  # negative element
     println(io, "$(length(hv))-element $(typeof(hv))")
@@ -20,4 +20,9 @@ end
 
 function Base.show(io::IO, hv::AbstractHV)
     return print(io, "$(length(hv))-element $(typeof(hv)) - m ± sd: $(round(mean(hv))) ± $(round(std(hv)))")
+end
+
+function Base.show(io::IO, hv::Union{BinaryHV,BipolarHV})
+    n = hv isa BinaryHV ? 0 : -1  # negative element
+    return print(io, "1 / $n : $(count(hv.v)) / $(length(hv) - count(hv.v))")
 end
