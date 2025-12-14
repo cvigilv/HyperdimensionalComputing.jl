952eb2894975cac2c09916747621937e66d52464
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-12-15 00:05:55 +0100

    feat: wip

--- src/representations.jl
+++ src/representations.jl
@@ -5,24 +5,82 @@ and a custom plotting recipe
 See ext/UnicodePlotting.jl for extensions based on UnicodePlotting
 =#
 
-function Base.show(io::IO, mime::MIME"text/plain", hv::AbstractHV)
-    println(io, "$(length(hv))-element $(typeof(hv))")
-    return println(io, "mean ± std : $(round(mean(hv), digits = 3)) ± $(round(std(hv), digits = 3))")
-end
+# ------------------------------------------------------------------------------------- Compact
+# function Base.show(io::IO, hv::TernaryHV)
+#     return print(io, "$(typeof(hv))(D=$(length(hv)), μ=$(round(mean(hv))), σ=$(round(std(hv))))")
+# end
+#
+# function Base.show(io::IO, hv::BinaryHV)
+#     D = length(hv.v)
+#     n_true = count(identity, hv.v)
+#     n_false = D - n_true
+#     return print(io, "BinaryHV(D=$(D), trues=$(n_true), falses=$(n_false))")
+# end
+#
+# function Base.show(io::IO, hv::BipolarHV)
+#     D = length(hv.v)
+#     n_true = count(identity, hv.v)
+#     n_false = D - n_true
+#     print(io, "BipolarHV(D=$(D), positives=$(n_true), negatives=$(n_false))")
+#     return
+# end
+#
+# # ------------------------------------------------------------------------------------- REPL
+# function Base.show(io::IO, mime::MIME"text/plain", hv::TernaryHV)
+#     println(io, "$(length(hv))-element $(typeof(hv)) with μ ± σ = $(round(mean(hv), digits = 3)) ± $(round(std(hv), digits = 3))")
+#     println(repr(mime, hv.v'; context=io))
+#     # return split(repr(mime, hv.v; context = io), '\n')[2:end] .|> println
+# end
+#
+# function Base.show(io::IO, mime::MIME"text/plain", hv::BinaryHV)
+#     counts = Dict(e => count(==(e), hv) for e in unique(hv))
+#     println(io, "$(length(hv))-element $(typeof(hv)) with $(counts[1]) trues and $(counts[0]) falses")
+#     return split(repr(mime, hv.v; context = io), '\n')[(begin + 1):end] .|> println
+# end
+#
+# function Base.show(io::IO, mime::MIME"text/plain", hv::BipolarHV)
+#     counts = Dict(e => count(==(e), hv) for e in unique(hv))
+#     println(io, "$(length(hv))-element $(typeof(hv)) with $(counts[1]) positives and $(counts[-1]) negatives")
+#     return split(repr(mime, hv.v .* 2 .- 1; context = io), '\n')[(begin + 1):end] .|> println
+# end
+function Base.show(io::IO, ::MIME"text/plain", hv::AbstractHV)
+    # NOTE: Based off https://github.com/JuliaLang/julia/blob/cf40898d56a5b32c6a2e97f61355440df36a7357/base/arrayshow.jl#L363
+    # Fast return for empty hypervectors
+    if isempty(hv) && (get(io, :compact, false)::Bool || hv isa AbstractHV)
+        return println(io, typeof(hv))
+    end
 
-function Base.show(io::IO, mime::MIME"text/plain", hv::Union{BinaryHV, BipolarHV})
-    counts = Dict(e => count(==(e), hv) for e in unique(hv))
-    n = hv isa BinaryHV ? 0 : -1  # negative element
-    println(io, "$(length(hv))-element $(typeof(hv))")
-    return println(io, "1 / $n : $(count(hv.v)) / $(length(hv) - count(hv.v))")
+    # 1) show summary before setting :compact
+    if typeof(hv) == BinaryHV
+        counts = Dict(e => count(==(e), hv) for e in unique(hv))
+        print(io, "$(length(hv))-element $(typeof(hv)) with $(counts[1]) true and $(counts[-1]) false")
+    elseif typeof(hv) == BipolarHV
+        counts = Dict(e => count(==(e), hv) for e in unique(hv))
+        print(io, "$(length(hv))-element $(typeof(hv)) with $(counts[1]) positives and $(counts[-1]) negatives")
+    else
+        print(io, "$(length(hv))-element $(typeof(hv)) with μ ± σ = $(round(mean(hv), digits = 3)) ± $(round(std(hv), digits = 3))")
+    end
+    isempty(hv) && return
+    print(io, ":")
 
-end
+    # 2) compute new IOContext
+    if !haskey(io, :compact) && length(axes(hv, 2)) > 1
+        io = IOContext(io, :compact => true)
+    end
+    if get(io, :limit, false)::Bool && eltype(hv) === Method
+        io = IOContext(io, :limit => false)
+    end
 
-function Base.show(io::IO, hv::AbstractHV)
-    return print(io, "$(length(hv))-element $(typeof(hv)) - m ± sd: $(round(mean(hv))) ± $(round(std(hv)))")
-end
+    if get(io, :limit, false)::Bool && displaysize(io)[1] - 4 <= 0
+        print(io, " …")
+    else
+        println(io)
+    end
 
-function Base.show(io::IO, hv::Union{BinaryHV, BipolarHV})
-    n = hv isa BinaryHV ? 0 : -1  # negative element
-    return print(io, "1 / $n : $(count(hv.v)) / $(length(hv) - count(hv.v))")
+    # 3) update typeinfo
+    io = IOContext(io, :typeinfo => eltype(hv))
+
+    # 4) show actual content
+    recur_io = IOContext(io, :SHOWN_SET => hv)
+    return Base.print_array(recur_io, hv)
 end
